ed43bd2e48decd9eb09fc5239696b1376adaecc7
romedu <32285662+romedu@users.noreply.github.com>
2019-04-16 20:21:28 -0400

    Add resume download link

--- src/containers/ContactPage/ContactPage.js
+++ src/containers/ContactPage/ContactPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import ArrowLink from "../../components/ArrowLink/ArrowLink";
 import IconLink from "../../components/IconLink/IconLink";
 import "./ContactPage.css";
+import cv from "../../assets/files/roberto-meran-cv-english.docx";
 
 const ContactPage = props => {
    const {availWidth: width} = window.screen;
@@ -18,6 +19,9 @@ const ContactPage = props => {
             <IconLink url="https://github.com/romedu" iconName="github" bigIcon />
             <IconLink url="https://www.linkedin.com/in/roberto-meran-duverge-68622b109/" iconName="linkedin" bigIcon />
          </div>
+         <a className="animated fadeIn DownloadButton" href={cv} download>
+            Download Resume
+         </a>
       </div>
    );
 }
