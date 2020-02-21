efd82303854876d3e2ef884c1c109f679c9c5549
romedu <romedu1@hotmail.com>
2020-02-20 22:57:32 -0400

    Updates

---
+++ precache-manifest.a46817397d29378dba29e1adeac818bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "949f50055aaa3311e114",
+    "url": "/static/css/main.e014bc88.chunk.css"
+  },
+  {
+    "revision": "949f50055aaa3311e114",
+    "url": "/static/js/main.949f5005.chunk.js"
+  },
+  {
+    "revision": "d6a71f41a91d96ea8778",
+    "url": "/static/css/1.3a542e3e.chunk.css"
+  },
+  {
+    "revision": "d6a71f41a91d96ea8778",
+    "url": "/static/js/1.d6a71f41.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "38b412b24d713b83b727453054f7e69b",
+    "url": "/static/media/roberto-meran-cv-english.38b412b2.docx"
+  },
+  {
+    "revision": "f807acc7cd2d5d0f8d9a83cfdaf79ff5",
+    "url": "/index.html"
+  }
+];
